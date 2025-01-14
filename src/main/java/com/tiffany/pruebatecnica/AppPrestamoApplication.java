0c330c7e4f2a264f48144761a2107bdd3a0e7382
ale <tiffanyalejandrina@gmail.com>
2025-01-13 20:00:18 -0600

    inicializacion de proyecto

---
+++ src/main/java/com/tiffany/pruebatecnica/AppPrestamoApplication.java
@@ -0,0 +1,13 @@
+package com.tiffany.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPrestamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPrestamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiffany/pruebatecnica/AppPrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiffany.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
