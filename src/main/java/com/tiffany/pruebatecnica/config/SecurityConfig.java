5ea4b518cd1ec0dbe45455dad7af16b014161e9b
TiffanyAlejandrina <tchaconc@miumg.edu.gt>
2025-01-17 21:49:21 -0600

    Se agregan direcciones desde donde se realizaran peticiones

--- src/main/java/com/tiffany/pruebatecnica/config/SecurityConfig.java
+++ src/main/java/com/tiffany/pruebatecnica/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization")); //
